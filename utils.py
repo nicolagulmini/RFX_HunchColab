2c064662dca3441bae6269538562c407375d90bf
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-09-29 09:43:54 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,16 @@
+from numpy import linspace
+from numpy import array
+from numpy.random import normal
+from scipy.stats import norm
+from matplotlib import pyplot as plt
+
+def generate_gaussian_mixture_curve(n_p=2000, n_g=2, mean_and_vars=[(0,1), (4,2)], start=-3, sample_period=.005, awgn_dev=.001, plot=False):
+    x = linspace(start, start+n_p*sample_period, n_p)
+    y = [sum([norm.pdf(el, mean_and_vars[i][0], mean_and_vars[i][1])+normal(0, awgn_dev) for i in range(n_g)]) for el in x]
+    if plot:
+        plt.plot(x, y)
+        plt.show()
+    return array(x+y)
+
+number_of_gaussians = 5
+generate_gaussian_mixture_curve(n_p=4000, n_g=number_of_gaussians, mean_and_vars=[(i*5, 1) for i in range(number_of_gaussians)], plot=True)

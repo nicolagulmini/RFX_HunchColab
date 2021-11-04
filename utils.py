a50c494cba3f81ea73972587118b4ead5b32395a
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-11-04 15:59:13 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,25 +12,17 @@ def generate_gaussian_mixture_curve_1(n_p=2000, n_g=2, mean_and_vars=[(0,1), (4,
         plt.show()
     return array(x.tolist()+y)
 
-def generate_gaussian_mixture_curve_2(n_p=100, mean_and_vars=[(0,0.01), (0.5,0.01)], awgn_dev=.001, plot=False):
+def generate_gaussian_mixture_curve_2(n_p=100, mean_and_vars=[(0,0.01), (0.5,0.01)], awgn_dev=.001, plot=False, bidim_points=False):
     x = linspace(0, 1, n_p)
     y = [sum([norm.pdf(el, mean_and_vars[i][0], mean_and_vars[i][1])+normal(0, awgn_dev) for i in range(len(mean_and_vars))]) for el in x]
     y = y/max(y)
     if plot:
         plt.plot(x, y)
         plt.show()
+    if bidim_points:
+        bidim_curve = []
+        for i in range(x.shape[0]):
+            point = array([x[i], y[i]])
+            bidim_curve.append(point)
+        return array(bidim_curve)
     return x, y
-
-''' test
-x, y = generate_gaussian_mixture_curve_2()
-print(type(x))
-print(type(y))
-print(x.shape)
-print(y.shape)
-bidim_curve = []
-for i in range(x.shape[0]):
-    point = array([x[i], y[i]])
-    bidim_curve.append(point)
-bidim_curve = array(bidim_curve)
-print(bidim_curve.shape)
-'''

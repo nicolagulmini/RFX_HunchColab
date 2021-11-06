3e881b1b62df62eb39e02774826234a07695cea8
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-11-06 13:15:42 +0100

    Update model.py

--- model.py
+++ model.py
@@ -55,7 +55,7 @@ def decoder(latent_dim, target_shape, name='decoder', summary=False):
     return decoder_model
 
 class VAE(keras.Model):
-    def __init__(self, input_shape, latent_dim, decoder, **kwargs):
+    def __init__(self, input_shape, latent_dim, **kwargs):
         super(VAE, self).__init__(**kwargs)
         self.encoder = encoder(input_shape, latent_dim)
         self.decoder = decoder(latent_dim, input_shape)

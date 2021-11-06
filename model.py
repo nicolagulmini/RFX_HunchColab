8fe2cf31d6a1b3f6e79eda14a921eb271402dbcb
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-11-06 18:27:34 +0100

    Update model.py

--- model.py
+++ model.py
@@ -67,8 +67,9 @@ def decoder(latent_dim, target_shape, name='decoder', summary=False):
     return decoder_model
 
 class VAE(keras.Model):
-    def __init__(self, input_shape, latent_dim, beta=1., **kwargs):
+    def __init__(self, input_shape, latent_dim=2, beta=1., **kwargs):
         super(VAE, self).__init__(**kwargs)
+        self.latent_dim = latent_dim
         self.encoder = encoder(input_shape, latent_dim)
         self.decoder = decoder(latent_dim, input_shape)
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")

abcb7bc25642e823dfd9f815619461dbf3c6ddb9
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-11-06 16:04:02 +0100

    Update model.py

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.callbacks import LearningRateScheduler
 
 class Sampling(layers.Layer):
 
